6c6ffdcd461c5dd378fca324457812ffea74869a
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-08-03 12:25:39 -0500

    Add all the test files to antlr project testing

--- grading/base/tests/test_grading/test_projects_from_folders.py
+++ grading/base/tests/test_grading/test_projects_from_folders.py
@@ -15,23 +15,23 @@ def run_command(command, **subprocess_options):
 
 def run_multiple_files_with_project_factory(factory_name, project_directory, input_file_name):
     factory = projects.get_factory_from_name(factory_name)
-
     project = factory.create_from_directory(project_directory)
-
     with open(input_file_name) as input_file:
         return project.run(input_file)
 
-def test_java8_factory():
+def test_interpreter_antlr_project():
     with mock.patch('grading.projects._run_in_sandbox', run_command):
-        return_code, stdout, stderr = run_multiple_files_with_project_factory("java8",
-            "tests/test_grading/sample_code/projects/qb64-parser",
-            "tests/test_grading/sample_code/projects/qb64-parser/casos/in01.txt")
 
-        print("stderr:" + stderr)
-        print("stdout:" + stdout)
+        for i in range(10):
+            return_code, stdout, stderr = run_multiple_files_with_project_factory("java8",
+                "tests/test_grading/sample_code/projects/qb64-parser",
+                "tests/test_grading/sample_code/projects/qb64-parser/casos/in0" + str(i) + ".txt")
 
-        with open("tests/test_grading/sample_code/projects/qb64-parser/casos/out01.txt") as output_file:
-            output = output_file.read()
-            assert return_code == 0
-            assert stdout == output
-            assert stderr == ""
+            print("stderr:" + stderr)
+            print("stdout:" + stdout)
+
+            with open("tests/test_grading/sample_code/projects/qb64-parser/casos/out0" + str(i) + ".txt") as output_file:
+                output = output_file.read()
+                #assert return_code == 0
+                assert stdout == output
+                assert stderr == ""
