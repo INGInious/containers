af9599f39fe731c5c1452f6173c16a011033b0e5
Andres Mauricio Rondon Patino <amrondonp@gmail.com>
2017-08-03 09:32:45 -0500

    Add Java factories basic test

--- grading/base/tests/test_grading/test_projects.py
+++ grading/base/tests/test_grading/test_projects.py
@@ -62,3 +62,23 @@ def test_cpp11_factory():
         assert return_code == 0
         assert stdout == "Hello world!\n"
         assert stderr == ""
+
+def test_java7_factory():
+    with mock.patch('grading.projects._run_in_sandbox', run_command):
+        return_code, stdout, stderr = run_code_with_project_factory("java7",
+            "tests/test_grading/sample_code/java7.java",
+            "tests/test_grading/sample_code/empty_input.txt")
+
+        assert return_code == 0
+        assert stdout == "Hello world!\n"
+        assert stderr == ""
+
+def test_java8_factory():
+    with mock.patch('grading.projects._run_in_sandbox', run_command):
+        return_code, stdout, stderr = run_code_with_project_factory("java8",
+            "tests/test_grading/sample_code/java8.java",
+            "tests/test_grading/sample_code/empty_input.txt")
+
+        assert return_code == 0
+        assert stdout == "Hello world!\n"
+        assert stderr == ""
