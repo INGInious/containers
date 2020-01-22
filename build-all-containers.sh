69d2a48468c4f46a8edf4f032040dbf94e7cfa65
Drumor <ludovic.taffin@uclouvain.be>
2020-01-22 11:06:05 +0100

    Hardcoded ugly fix : Force cpp as first container

--- build-all-containers.sh
+++ build-all-containers.sh
@@ -3,6 +3,10 @@ set -e
 
 grading=$(ls -1d grading/*/ | rev | cut -c 2- | rev | sed 's/^grading\/\(.*\)$/\1/')
 
+# Be sure to build cpp first to solve dependance issue.
+# Ugly temporary fix 
+docker build -t "ingi/inginious-c-cpp" "grading/cpp"
+
 for container in $grading; do
         echo "-------------------------------------------------------"
         echo "- Building grading container $container"
